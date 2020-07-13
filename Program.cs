edfc1d59cb1626e3a030b8e243cdc133ec4b2d94
emandere <emandere@gmail.com>
2020-07-13 05:52:13 -0400

    initial setup

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace forex_quotes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
